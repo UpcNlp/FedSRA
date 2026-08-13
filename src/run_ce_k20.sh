1b5fb1f86ce907c1d7f9c03088137330e97477f2
dongshou <2321084428@qq.com>
2026-08-14 03:54:06 +0800

    Prune one-off exploratory scripts from src/ (keep the reproduction path)

--- src/run_ce_k20.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /public/home/dongshou/fedETF/ETF-pesuade
-export LD_LIBRARY_PATH='/opt/dtk/cuda/cuda-12/lib64:/usr/local/lib/python3.10/dist-packages/torch/lib:/opt/ucx/lib:/opt/dtk/hip/lib:/opt/dtk/llvm/lib:/opt/dtk/lib:/opt/dtk/lib64:/opt/hyhal/lib:/opt/hyhal/lib64:/opt/mpi/lib:/opt/hwloc/lib:' HIP_PATH=/opt/dtk ROCM_PATH=/opt/dtk
-export HIP_VISIBLE_DEVICES=2 CUDA_VISIBLE_DEVICES=2
-/public/home/dongshou/anaconda/envs/ct/bin/python -u ce_train.py --alpha 0.05 --K 20 --epochs 150 --out saved_models/ce_a0.05_k20_s42
--- src/run_export_a05.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /opt/dtk/env.sh
-source /public/home/dongshou/anaconda/etc/profile.d/conda.sh
-conda activate ct
-cd /public/home/dongshou/fedETF/ETF-pesuade
-export HIP_VISIBLE_DEVICES=5 CUDA_VISIBLE_DEVICES=5
-python -u export_align_feats.py --alpha 0.5 --K 10 --ckpt_dir saved_models/a0.5_k10_s42 --tag ERL --out tmp/align_ERL_a0.5_k10.npz
-echo ALLDONE05
